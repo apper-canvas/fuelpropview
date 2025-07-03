5f948bfaa2abd71a3929b72f41b33ca8c6ae498f
RishabhCompanyHub <rishabh@companyhub.com>
2025-07-03 13:06:27 +0530

    Update Header.jsx

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
               <ApperIcon name="Home" size={24} className="text-white" />
             </div>
             <span className="text-2xl font-display font-bold gradient-text">
-              PropView 2
+              PropView 3
             </span>
           </Link>
 
